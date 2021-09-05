2538d33a1226d555fdb0fff311e5fb9b4a96fd97
leehan416 <leehan416@naver.com>
2021-09-05 21:30:08 +0900

    join HanGyeol lee

--- lib/GithubTest.dart
+++ lib/GithubTest.dart
@@ -1,4 +1,5 @@
 // For github test
 
-// if you edit your file, please write your current nickname and realname here...
+// if you edit your file, please write your current nickname and real name here...
 // 1. juyoungryan - 하주영
+// 2. leehan416 - 이한결
