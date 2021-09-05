aa3e56e6240a934231c29c926ef29ab8d4e2d5b8
luiheid <hellolisa99@naver.com>
2021-09-05 21:37:49 +0900

    join hyerim

--- lib/GithubTest.dart
+++ lib/GithubTest.dart
@@ -1,6 +1,5 @@
 // For github test
 
-// if you edit your file, please write your current nickname and real name here...
-// 1. juyoungryan - 하주영
-// 2. leehan416 - 이한결
+// if you edit your file, please write your current nickname and realname here...
+
 // 3. yeob-yi - 강신엽
