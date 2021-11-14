de66106bbd7e57abe31702e9c517d666e73dc5c3
Yeob <happyk100@naver.com>
2021-11-14 20:28:48 +0900

    commit by yeob in 2021/11/08  - notification icon deleted from chatting page

--- lib/home/chatListAppBar.dart
+++ lib/home/chatListAppBar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/notification/notificationList.dart';
 
 class ChatListAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
@@ -18,19 +17,6 @@ class ChatListAppBar extends StatelessWidget implements PreferredSizeWidget {
           textScaleFactor: 1.4,
         ),
         centerTitle: true,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-            child: IconButton(
-                icon: Image.asset("assets/notice.png", width: 73, height: 76, scale: 3),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NotificationListPage()),
-                  );
-                }),
-          ),
-        ],
         backgroundColor: Color(0xff6990FF));
   }
 }
